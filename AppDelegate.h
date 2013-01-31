b01c9737fb7aca87f263e585d75485310f6847d9
yogaaaaa@yahoo.com <yogaaaaa@yahoo.com@e86723fe-27c7-f596-5aa3-d1ee9c7e0e01>
2013-01-31 18:38:17 +0000

    initial

---
+++ AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GarbageCollector
+//
+//  Created by Student06 on 1/31/13.
+//  Copyright (c) 2013 MMAcademy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
