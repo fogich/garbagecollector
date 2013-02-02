02cf4fa27e236052c1437766314ff7221bd325cc
ShadowoverCliff@gmail.com <ShadowoverCliff@gmail.com@e86723fe-27c7-f596-5aa3-d1ee9c7e0e01>
2013-02-02 10:29:53 +0000

    Importing MapViewController.h

---
+++ trunk/GarbageCollector/MapViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapViewController.h
+//  GarbageCollector
+//
+//  Created by Student09 on 2/2/13.
+//  Copyright (c) 2013 MMAcademy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewController : UIViewController
+
+@end
