a0eff6ecf837a752a9893434ac6d01b9689d05f3
fogich <fogich@gmail.com>
2013-02-02 12:27:06 +0000

    Importing TestClass.h

---
+++ GarbageCollector/TestClass.h
@@ -0,0 +1,13 @@
+//
+//  TestClass.h
+//  GarbageCollector
+//
+//  Created by Ognian.Chirkov on 2/2/13.
+//  Copyright (c) 2013 MMAcademy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestClass : NSObject
+
+@end
