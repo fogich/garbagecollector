b8ec99b918b91ff55e2250a8b515c6f761c763ed
yogaaaaa <yogaaaaa@yahoo.com>
2013-02-02 13:11:05 +0000

    Added garbage storage classes.

---
+++ GarbageCollector/GarbageStorage.h
@@ -0,0 +1,13 @@
+//
+//  GarbageStorage.h
+//  GarbageCollector
+//
+//  Created by Student06 on 2/2/13.
+//  Copyright (c) 2013 MMAcademy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GarbageStorage : NSObject
+
+@end
