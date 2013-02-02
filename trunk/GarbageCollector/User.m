ccca0e41e8ce57d20f620e768fbc7f5281d8dba8
yogaaaaa@yahoo.com <yogaaaaa@yahoo.com@e86723fe-27c7-f596-5aa3-d1ee9c7e0e01>
2013-02-02 12:38:25 +0000

    Adding 6 files for models

---
+++ trunk/GarbageCollector/User.m
@@ -0,0 +1,20 @@
+//
+//  User.m
+//  GarbageCollector
+//
+//  Created by Student06 on 2/2/13.
+//  Copyright (c) 2013 MMAcademy. All rights reserved.
+//
+
+#import "User.h"
+#import "GarbageSpot.h"
+
+
+@implementation User
+
+@dynamic fbid;
+@dynamic fbName;
+@dynamic fbPictureFilename;
+@dynamic reports;
+
+@end
