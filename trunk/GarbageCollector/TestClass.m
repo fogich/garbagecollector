68b799c7e1829a1b7cc9f3856c15f54d222b35b6
fogich@gmail.com <fogich@gmail.com@e86723fe-27c7-f596-5aa3-d1ee9c7e0e01>
2013-02-02 12:27:23 +0000

    Importing TestClass.m

---
+++ trunk/GarbageCollector/TestClass.m
@@ -0,0 +1,13 @@
+//
+//  TestClass.m
+//  GarbageCollector
+//
+//  Created by Ognian.Chirkov on 2/2/13.
+//  Copyright (c) 2013 MMAcademy. All rights reserved.
+//
+
+#import "TestClass.h"
+
+@implementation TestClass
+
+@end
