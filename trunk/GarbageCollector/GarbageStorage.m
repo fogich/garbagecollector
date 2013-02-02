d9adbb5c246423a36c62531abb9ab974b8dcb026
yogaaaaa@yahoo.com <yogaaaaa@yahoo.com@e86723fe-27c7-f596-5aa3-d1ee9c7e0e01>
2013-02-02 13:11:05 +0000

    Added garbage storage classes.

---
+++ trunk/GarbageCollector/GarbageStorage.m
@@ -0,0 +1,13 @@
+//
+//  GarbageStorage.m
+//  GarbageCollector
+//
+//  Created by Student06 on 2/2/13.
+//  Copyright (c) 2013 MMAcademy. All rights reserved.
+//
+
+#import "GarbageStorage.h"
+
+@implementation GarbageStorage
+
+@end
