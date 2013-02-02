cf1e4febf93d58caf15c577fdadd59a0fcfc2083
yogaaaaa <yogaaaaa@yahoo.com>
2013-02-02 17:43:08 +0000

    Changed the map controller a bit.

--- GarbageCollector/MapViewController.m
+++ GarbageCollector/MapViewController.m
@@ -7,15 +7,16 @@
 //
 
 #import "MapViewController.h"
+#import "PinAnnotation.h"
 
 @interface MapViewController ()
 
-@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+@property (weak, nonatomic) IBOutlet MKMapView *map;
 
 @end
 
 @implementation MapViewController
-@synthesize mapView;
+@synthesize map;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -43,12 +44,99 @@
     self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"images.jpg"] style:UIBarButtonItemStylePlain target:self action:nil];
 	// Do any additional setup after loading the view.
     
+    //initializing map
+    self.map.mapType = MKMapTypeStandard;
     
+    NSMutableArray* annots = [NSMutableArray array];
+    PinAnnotation* p = [[PinAnnotation alloc] init];
+    p.name = @"Jim Beam Center";
+    p.subName = @"Tel : 0878 454647";
+    p.x = 42.691126;
+    p.y = 23.319875;
+    [annots addObject: p];
+    
+    PinAnnotation* p1 = [[PinAnnotation alloc] init];
+    p1.name = @"Rock N Rolla";
+    p1.subName = @"Tel: 0886 242020";
+    p1.x = 42.694442;
+    p1.y = 23.322512;
+    [annots addObject: p1];
+    
+    PinAnnotation* p2 = [[PinAnnotation alloc] init];
+    p2.name = @"Rock it";
+    p2.subName = @"Tel: 09898703535";
+    p2.x = 42.698692;
+    p2.y = 23.328907;
+    [annots addObject: p2];
+
+    self.map.delegate = self;
+    [self.map addAnnotations: annots];
 }
+
+-(void)viewWillAppear:(BOOL)animated
+{
+    [self.map setCenterCoordinate: self.map.userLocation.location.coordinate animated:NO];
+    
+    CLLocationCoordinate2D defultLocation;
+    
+    defultLocation.latitude = 42.691126;
+    defultLocation.longitude = 23.319875;
+    
+    MKCoordinateRegion defaultRegion = MKCoordinateRegionMakeWithDistance(defultLocation, 800, 800);
+    [self.map setRegion:defaultRegion animated:YES];
+    
+//    self.map.region =
+//    MKCoordinateRegionMake(CLLocationCoordinate2DMake(42.691126,23.319875), MKCoordinateSpanMake(0.05, 0.05));
+}
+
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+-(MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation
+{
+    MKPinAnnotationView *pinView = (MKPinAnnotationView *)[mapView dequeueReusableAnnotationViewWithIdentifier:@"pinView"];
+    if (!pinView) {
+        pinView = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:@"pinView"];
+        pinView.canShowCallout = YES;
+        
+        UIButton* b = [UIButton buttonWithType:UIButtonTypeDetailDisclosure];
+        b.tag = 1;
+        pinView.leftCalloutAccessoryView = b;
+        
+        UIButton* b2 = [UIButton buttonWithType:UIButtonTypeDetailDisclosure];
+        b2.tag = 2;
+        pinView.rightCalloutAccessoryView = b2;
+    }
+    else
+    {
+        //PinAnnotation* pA = (PinAnnotation*)annotation;
+        //pinView.annotation = pA;
+    }
+    
+    return pinView;
+}
+
+-(void)mapView:(MKMapView *)mapView annotationView:(MKAnnotationView *)view calloutAccessoryControlTapped:(UIControl *)control
+{
+    if(control.tag == 1)
+    {
+        PinAnnotation* pinAnotation = (PinAnnotation*)view.annotation;
+        
+        NSString* message = [NSString stringWithFormat:@"%@ %@", pinAnotation.title, pinAnotation.subtitle];
+        
+        UIAlertView* alert = [[UIAlertView alloc] initWithTitle:@"Alert" message:message delegate:self cancelButtonTitle:@"OK" otherButtonTitles: nil];
+        [alert show];
+    }
+    else
+    {
+        UIAlertView* alert = [[UIAlertView alloc] initWithTitle:@"Alert" message:@"Table Reserved" delegate:self cancelButtonTitle:@"OK" otherButtonTitles: nil];
+        [alert show];
+    }
+}
+
+
 
 @end
