6591aab991629d8525320e6dd94117e5c14a6c3c
yogaaaaa <yogaaaaa@yahoo.com>
2013-02-02 17:40:50 +0000

    Added pin annotation

---
+++ GarbageCollector/PinAnnotation.h
@@ -0,0 +1,20 @@
+//
+//  PinAnnotation.h
+//  ClubPoints
+//
+//  Created by Student06 on 1/21/13.
+//  Copyright (c) 2013 MMAcademy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <MapKit/MapKit.h>
+
+@interface PinAnnotation : NSObject<MKAnnotation>
+
+@property float x;
+@property float y;
+@property NSString* name;
+@property NSString* subName;
+
+
+@end
