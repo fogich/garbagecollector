94a58f30cac33c2ae4c9e12dbd98a4037fe86878
ShadowoverCliff <ShadowoverCliff@gmail.com>
2013-02-02 10:29:40 +0000

    Importing MainTableViewController.h

---
+++ GarbageCollector/MainTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainTableViewController.h
+//  GarbageCollector
+//
+//  Created by Student09 on 2/2/13.
+//  Copyright (c) 2013 MMAcademy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainTableViewController : UITableViewController
+
+@end
