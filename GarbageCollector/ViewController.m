f1be5ef2848cc0999ca9eae20900cdbcc0123dde
ShadowoverCliff <ShadowoverCliff@gmail.com>
2013-02-02 10:23:14 +0000

    New Storyboard shit and view Controllers for it

--- GarbageCollector/ViewController.m
+++ GarbageCollector/ViewController.m
@@ -18,8 +18,18 @@
 {
     [super viewDidLoad];
     
-    self.navigationItem.title=@"FacebookProfile";
-    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"images.jpg"] style:UIBarButtonItemStylePlain target:self action:nil];
+    self.navigationItem.title=@"Facebook";
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"someShittyImageYouMustDownloadFromFacebok"] style:UIBarButtonItemStylePlain target:self action:nil];
+    UIBarButtonItem* butt= [[UIBarButtonItem alloc] initWithTitle:@"Map" style:UIBarButtonItemStyleBordered target:self action:@selector(switchToMapScreen)];
+    UIBarButtonItem* butt1= [[UIBarButtonItem alloc] initWithTitle:@"Table" style:UIBarButtonItemStyleBordered target:self action:@selector(switchToTableScreen)];
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 120, 30)];
+    //set some custom font for the label
+	[label setFont:[UIFont fontWithName:@"Helvetica" size:18]];
+	[label setBackgroundColor:[UIColor clearColor]];
+	[label setTextColor:[UIColor blackColor]];
+	[label setText:@"Facebook Profile"];
+	[self.navigationController.navigationBar.topItem setTitleView:label];
+    self.navigationItem.rightBarButtonItems = [NSArray arrayWithObjects:butt,butt1 ,nil];
 	// Do any additional setup after loading the view, typically from a nib.
     
     //some new comments
@@ -35,5 +45,13 @@
     // Dispose of any resources that can be recreated.
 
 }
+-(void) switchToTableScreen
+{
 
+    
+}
+-(void) switchToMapScreen
+{
+    
+}
 @end
