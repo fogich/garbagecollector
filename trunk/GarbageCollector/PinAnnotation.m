8b5bad4e66902c79893a2ad0d42e58c48a16746b
yogaaaaa@yahoo.com <yogaaaaa@yahoo.com@e86723fe-27c7-f596-5aa3-d1ee9c7e0e01>
2013-02-02 17:40:50 +0000

    Added pin annotation

---
+++ trunk/GarbageCollector/PinAnnotation.m
@@ -0,0 +1,32 @@
+//
+//  PinAnnotation.m
+//  ClubPoints
+//
+//  Created by Student06 on 1/21/13.
+//  Copyright (c) 2013 MMAcademy. All rights reserved.
+//
+
+#import "PinAnnotation.h"
+
+@implementation PinAnnotation
+@synthesize x;
+@synthesize y;
+@synthesize name;
+@synthesize subName;
+
+-(CLLocationCoordinate2D)coordinate
+{
+    return CLLocationCoordinate2DMake(self.x, self.y);
+}
+
+-(NSString *)title
+{
+    return self.name;
+}
+
+-(NSString *)subtitle
+{
+   return self.subName;
+}
+
+@end
