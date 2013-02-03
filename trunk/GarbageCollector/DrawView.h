d6c4d736b36795dd24593534d3c84ad7ad8dd4b2
fogich@gmail.com <fogich@gmail.com@e86723fe-27c7-f596-5aa3-d1ee9c7e0e01>
2013-02-03 01:38:15 +0000

    Functional version of Pie Chart. Heavily needs refactoring.... Still Kuci is with us!!! :)

---
+++ trunk/GarbageCollector/DrawView.h
@@ -0,0 +1,13 @@
+//
+//  DrawView.h
+//  DrawDemo
+//
+//  Created by Ognian.Chirkov on 17/1/13.
+//  Copyright (c) 2013 MMAcademy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DrawView : UIView
+
+@end
