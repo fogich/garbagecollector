2b3c67be1520c2731af053da240bc7d9fecfe641
yogaaaaa@yahoo.com <yogaaaaa@yahoo.com@e86723fe-27c7-f596-5aa3-d1ee9c7e0e01>
2013-02-05 14:41:05 +0000

    Added show depo view controller.

---
+++ trunk/GarbageCollector/ShowDepoViewController.m
@@ -0,0 +1,38 @@
+//
+//  ShowDepoViewController.m
+//  GarbageCollector
+//
+//  Created by Student06 on 2/5/13.
+//  Copyright (c) 2013 MMAcademy. All rights reserved.
+//
+
+#import "ShowDepoViewController.h"
+
+@interface ShowDepoViewController ()
+
+@end
+
+@implementation ShowDepoViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
