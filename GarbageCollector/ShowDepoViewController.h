163d64622d10b9810c8ed734827b1b54ae06a66b
yogaaaaa <yogaaaaa@yahoo.com>
2013-02-05 14:41:05 +0000

    Added show depo view controller.

---
+++ GarbageCollector/ShowDepoViewController.h
@@ -0,0 +1,13 @@
+//
+//  ShowDepoViewController.h
+//  GarbageCollector
+//
+//  Created by Student06 on 2/5/13.
+//  Copyright (c) 2013 MMAcademy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ShowDepoViewController : UIViewController
+
+@end
