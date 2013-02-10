268b5de3d72b773b48ed8e009a90dc29e2a5c253
yogaaaaa <yogaaaaa@yahoo.com>
2013-02-10 14:50:08 +0000

    Added DataGenerator.

---
+++ GarbageCollector/DataGenerator.h
@@ -0,0 +1,13 @@
+//
+//  DataGenerator.h
+//  GarbageCollector
+//
+//  Created by Student06 on 2/10/13.
+//  Copyright (c) 2013 MMAcademy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DataGenerator : NSObject
+
+@end
---
+++ GarbageCollector/DataGenerator.m
@@ -0,0 +1,13 @@
+//
+//  DataGenerator.m
+//  GarbageCollector
+//
+//  Created by Student06 on 2/10/13.
+//  Copyright (c) 2013 MMAcademy. All rights reserved.
+//
+
+#import "DataGenerator.h"
+
+@implementation DataGenerator
+
+@end
