e8665a1c02b32e2873cdcb1dabb1d13aaa27b6fa
yogaaaaa@yahoo.com <yogaaaaa@yahoo.com@e86723fe-27c7-f596-5aa3-d1ee9c7e0e01>
2013-02-11 13:54:31 +0000

    added stats view controller.

---
+++ trunk/GarbageCollector/StatsViewController.h
@@ -0,0 +1,13 @@
+//
+//  StatsViewController.h
+//  GarbageCollector
+//
+//  Created by Student06 on 2/11/13.
+//  Copyright (c) 2013 MMAcademy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StatsViewController : UIViewController
+
+@end
---
+++ trunk/GarbageCollector/StatsViewController.m
@@ -0,0 +1,45 @@
+//
+//  StatsViewController.m
+//  GarbageCollector
+//
+//  Created by Student06 on 2/11/13.
+//  Copyright (c) 2013 MMAcademy. All rights reserved.
+//
+
+#import "StatsViewController.h"
+
+@interface StatsViewController ()
+
+@end
+
+@implementation StatsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+-(void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
