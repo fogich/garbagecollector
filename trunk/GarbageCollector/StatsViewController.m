df4590c3b6016f27fd3abe3a1781a3b12162c717
yogaaaaa@yahoo.com <yogaaaaa@yahoo.com@e86723fe-27c7-f596-5aa3-d1ee9c7e0e01>
2013-02-11 14:25:04 +0000

    Updated storyboard.

--- trunk/GarbageCollector/StatsViewController.m
+++ trunk/GarbageCollector/StatsViewController.m
@@ -7,8 +7,15 @@
 //
 
 #import "StatsViewController.h"
+#import "DrawView.h"
+#import "AppDelegate.h"
 
 @interface StatsViewController ()
+@property (weak, nonatomic) IBOutlet DrawView *graphicsView;
+- (IBAction)monthClick:(id)sender;
+- (IBAction)weekClick:(id)sender;
+- (IBAction)topClick:(id)sender;
+
 
 @end
 
@@ -27,13 +34,23 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
-}
-
--(void)viewWillAppear:(BOOL)animated
-{
-    [super viewWillAppear:animated];
     
-    
+    UIBarButtonItem* backButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemReply target:self action:@selector(returnToPreviousScreen)];
+    self.navigationItem.leftBarButtonItems = [NSArray arrayWithObject:backButton];
+    self.navigationItem.hidesBackButton=YES;
+    UIView *myView = [[UIView alloc] initWithFrame: CGRectMake(0, 0, self.view.window.bounds.size.width, 30)];
+    UILabel *title = [[UILabel alloc] initWithFrame: CGRectMake((self.view.window.bounds.size.width/2)-40, 0, 160, 30)];
+    title.text = [(AppDelegate *)[[UIApplication sharedApplication] delegate] userName];
+    [title setTextColor:[UIColor blackColor]];
+    [title setFont:[UIFont boldSystemFontOfSize:16.0]];
+    [title setBackgroundColor:[UIColor clearColor]];
+    UIImage *image = [(AppDelegate *)[[UIApplication sharedApplication] delegate] profilePicture];
+    UIImageView *myImageView = [[UIImageView alloc] initWithImage:image];
+    myImageView.frame = CGRectMake((self.view.window.bounds.size.width/2)-80, 0, 30, 30);
+    [myView addSubview:title];
+    [myView setBackgroundColor:[UIColor  clearColor]];
+    [myView addSubview:myImageView];
+    self.navigationItem.titleView = myView;
 }
 
 - (void)didReceiveMemoryWarning
@@ -42,4 +59,12 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)monthClick:(id)sender {
+}
+
+- (IBAction)weekClick:(id)sender {
+}
+
+- (IBAction)topClick:(id)sender {
+}
 @end
