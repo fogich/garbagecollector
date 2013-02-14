f06392fde9d2a322e48058933d46f22faf8615ad
fogich <fogich@gmail.com>
2013-02-14 14:06:29 +0000

    post-presentation - added little animation on the iPhone version :)

--- GarbageCollector/StatsViewController.m
+++ GarbageCollector/StatsViewController.m
@@ -17,6 +17,9 @@
 - (IBAction)weekClick:(id)sender;
 - (IBAction)topClick:(id)sender;
 
+@property (strong, nonatomic) NSTimer *aTimer;
+@property (strong, nonatomic) NSMutableDictionary *finalData;
+@property (strong, nonatomic) NSMutableDictionary *currentData;
 
 @end
 
@@ -27,6 +30,8 @@
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // Custom initialization
+//        self.finalData = [NSMutableDictionary new];
+//        self.currentData = [NSMutableDictionary new];
     }
     return self;
 }
@@ -52,12 +57,94 @@
     [myView setBackgroundColor:[UIColor  clearColor]];
     [myView addSubview:myImageView];
     self.navigationItem.titleView = myView;
+    
+
+}
+
+-(void)viewDidAppear:(BOOL)animated{
+
+    //
+    self.finalData = [NSMutableDictionary new];
+    self.currentData = [NSMutableDictionary new];
+    [self defaultStat];
+
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+//NSTimer *aTimer = [NSTimer scheduledTimerWithTimeInterval: 2.0
+//                                                   target: self
+//                                                 selector:@selector(onTick:)
+//                                                 userInfo: nil repeats:YES];
+//
+
+
+
+
+-(void)defaultStat{
+
+    [self weekClick:nil];
+    //[self animate];
+
+}
+- (IBAction)animate:(id)sender {
+
+
+    self.aTimer = [NSTimer scheduledTimerWithTimeInterval: 0.01 target: self selector:@selector(onTick:) userInfo: nil repeats:YES];
+
+    NSArray *labels = @[@"Mon",@"Tue",@"Wed",@"Thu",@"Fri",@"Sat",@"Sun"];
+    
+    for (NSString *str in labels) {
+        
+        [self.finalData setValue:@(arc4random() % 10) forKey:str];
+        [self.currentData setValue:@0 forKey:str];
+
+    }
+
+
+}
+
+-(void)onTick:(NSTimer *)timer {
+    //This is where you need to set your stuff
+    
+    CGRect frame = CGRectMake(0, 0, self.graphicsView.frame.size.width, self.graphicsView.frame.size.height);
+    
+    GraphView *monthlyView = [[GraphView alloc] initWithFrame:frame];
+
+
+    for (NSString *str in [self.finalData allKeys]) {
+        
+        NSLog(@"Str: %@ Current: %@  =? Final: %@", str,[self.currentData valueForKey:str] , [self.finalData valueForKey:str]);
+        
+        if ([[self.currentData valueForKey:str] intValue] < [[self.finalData valueForKey:str] intValue]) {
+            NSNumber *val = [NSNumber numberWithInt:[[self.currentData valueForKey:str] intValue]+1];
+            [self.currentData setValue:val forKey:str];
+        }
+        
+    }
+    
+    
+    monthlyView.data = self.currentData;
+    monthlyView.backgroundColor = [UIColor whiteColor];
+    
+    if ([self.graphicsView.subviews count] > 0) {
+        [[self.graphicsView.subviews objectAtIndex:0] removeFromSuperview];
+    }
+    
+    [self.graphicsView addSubview: monthlyView];
+    [monthlyView setNeedsDisplay];
+    
+    NSLog(@"Current data(onTick): %@", self.currentData);
+    NSLog(@"Final data(onTick): %@", self.finalData);
+    
+    if ([self.currentData isEqualToDictionary:self.finalData]) {
+        [timer invalidate];
+    }
+    
 }
 
 -(void) returnToPreviousScreen
@@ -78,6 +165,13 @@
 
     monthlyView.data = randomData;
     monthlyView.backgroundColor = [UIColor whiteColor];
+    
+    NSLog(@"%@",[self.graphicsView subviews]);
+    
+    if ([self.graphicsView.subviews count] > 0) {
+        [[self.graphicsView.subviews objectAtIndex:0] removeFromSuperview];
+    }
+    
     [self.graphicsView addSubview: monthlyView];
     [monthlyView setNeedsDisplay];
     NSLog(@"Monthly");
@@ -101,8 +195,14 @@
     
     weeklyView.data = randomData;
     weeklyView.backgroundColor = [UIColor whiteColor];
+    
+    NSLog(@"%@",[self.graphicsView subviews]);
+    
+    if ([self.graphicsView.subviews count] > 0) {
+        [[self.graphicsView.subviews objectAtIndex:0] removeFromSuperview];
+    }
+    
     [self.graphicsView addSubview: weeklyView];
-    //[monthlyView drawRect:nil];
     [weeklyView setNeedsDisplay];
     
     NSLog(@"Top");
@@ -123,6 +223,16 @@
     
     topView.data = randomData;
     topView.backgroundColor = [UIColor whiteColor];
+    NSLog(@"%@",[self.graphicsView subviews]);
+    
+    NSLog(@"count: %u", [self.graphicsView.subviews count]);
+        
+    if ([self.graphicsView.subviews count] > 0) {
+         [[self.graphicsView.subviews objectAtIndex:0] removeFromSuperview];
+    }
+    
+
+    
     [self.graphicsView addSubview: topView];
     //[monthlyView drawRect:nil];
     [topView setNeedsDisplay];
